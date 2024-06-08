8fa14c37c8ff5644baa18a44550bdbfd230c5522
yeshu10 <yeshvigupta10@gmail.com>
2024-06-09 01:37:52 +0530

    Pinkaddedinheart

--- src/pages/allproducts/AllProducts.jsx
+++ src/pages/allproducts/AllProducts.jsx
@@ -5,6 +5,8 @@ import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToCart } from '../../redux/cartSlice'
+import { addToWishlist } from '../../redux/wishlistSlice'
+import { toast } from 'react-toastify';
 
 function Allproducts() {
   const context = useContext(myContext)
@@ -13,18 +15,26 @@ function Allproducts() {
 
   const dispatch = useDispatch()
   const cartItems = useSelector((state)=> state.cart);
+  const wishlistItems = useSelector((state) => state.wishlist)
   console.log(cartItems)
 
   const addCart = (product)=> {
       dispatch(addToCart(product));
       toast.success('add to cart');
-
   }
+  const addWishlist = (products) => {
+    dispatch(addToWishlist(products))
+    toast.success('Added to wishlist');
+    console.log(products)
+}
 
   useEffect(() => {
       localStorage.setItem('cart', JSON.stringify(cartItems));
   }, [cartItems])
-  
+  useEffect(() => {
+    localStorage.setItem('wishlist', JSON.stringify(wishlistItems));
+}, [wishlistItems])
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
@@ -44,22 +54,35 @@ function Allproducts() {
                      .filter((obj) => obj.category.toLowerCase().includes(filterType))
                      .filter((obj) => obj.price.includes(filterPrice)).map((item, index) => {
                         const { title, price, description, imageUrl,id } = item;
+                        const isItemInWishlist = wishlistItems.some((wishlistItem) => wishlistItem.id === id);
+
                         return (
-                            <div onClick={()=> window.location.href = `/productinfo/${id}`}   key={index} className="p-4 md:w-1/4  drop-shadow-lg " >
+                            <div  key={index} className="p-4 md:w-1/4  drop-shadow-lg " >
                                 <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                                     <div className="flex justify-center cursor-pointer" >
-                                        <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src={imageUrl} alt="blog" />
+                                        <img onClick={()=> window.location.href = `/productinfo/${id}`}  className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src={imageUrl} alt="blog" />
                                     </div>
                                     <div className="p-5 border-t-2">
                                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>SarvHaat</h2>
                                         <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>{title}</h1>
                                         {/* <p className="leading-relaxed mb-3">{item.description.}</p> */}
                                         <p className="leading-relaxed mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{price}</p>
-                                        <div className=" flex justify-center">
+                                        <div className=" flex justify-between">
                                             <button type="button" 
                                             onClick={()=> addCart(item)}
                                             className="focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2">Add To Cart</button>
-
+                                            <button onClick={()=>addWishlist(item)} className={`rounded-full w-10 h-10 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4 ${isItemInWishlist ? 'bg-pink-500 text-white' : 'bg-gray-200'}`}>
+                                            <svg
+                                                    fill="currentColor"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth={2}
+                                                    className="w-5 h-5"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
+                                                </svg>
+    </button>
                                         </div>
                                     </div>
 
--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -34,7 +34,8 @@ function ProductInfo() {
 
     const dispatch = useDispatch()
     const cartItems = useSelector((state) => state.cart)
-    const wishlistItems = useSelector((state) => state.wishlist)
+    const wishlistItems = useSelector((state) => state.wishlist);
+    const isItemInWishlist = wishlistItems.some((item) => item.id === products.id);
 
     // add to cart
     const addCart = (products) => {
@@ -184,7 +185,7 @@ function ProductInfo() {
 <button  onClick={()=>addCart(products)} className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
     Add To Cart
 </button>
-<button onClick={()=>addWishlist(products)} className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+{/* <button onClick={()=>addWishlist(products)} className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
     <svg
         fill="currentColor"
         strokeLinecap="round"
@@ -195,6 +196,19 @@ function ProductInfo() {
     >
         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
     </svg>
+</button> */}
+<button onClick={()=>addWishlist(products)} className={`rounded-full w-10 h-10 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4 ${isItemInWishlist ? 'bg-pink-500 text-white' : 'bg-gray-200'}`}>
+<svg
+        fill="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        className="w-5 h-5"
+        viewBox="0 0 24 24"
+    >
+        <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
+    </svg>
+
 </button>
 </div>
 </div>
