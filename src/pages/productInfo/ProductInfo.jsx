1e3d7ceb4d8f8e52915216ea5223643ea97485d6
yeshu10 <yeshvigupta10@gmail.com>
2024-06-08 22:42:05 +0530

    ADDED TO WISHLIST

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { doc, getDoc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import { addToCart } from '../../redux/cartSlice';
+import { addToWishlist } from '../../redux/wishlistSlice';
 import { fireDB } from '../../firebase/FirebaseConfig';
 
 function ProductInfo() {
@@ -14,15 +15,12 @@ function ProductInfo() {
 
     const [products, setProducts] = useState('')
     const params = useParams()
-    // console.log(products.title)
 
     const getProductData = async () => {
         setLoading(true)
         try {
             const productTemp = await getDoc(doc(fireDB, "products", params.id))
-            // console.log(productTemp)
             setProducts(productTemp.data());
-            // console.log(productTemp.data())
             setLoading(false)
         } catch (error) {
             console.log(error)
@@ -30,30 +28,34 @@ function ProductInfo() {
         }
     }
 
-
     useEffect(() => {
         getProductData()
-
     }, [])
-
-
 
     const dispatch = useDispatch()
     const cartItems = useSelector((state) => state.cart)
-    // console.log(cartItems)
+    const wishlistItems = useSelector((state) => state.wishlist)
 
     // add to cart
     const addCart = (products) => {
         dispatch(addToCart(products))
-        toast.success('add to cart');
+        toast.success('Added to cart');
+    }
+
+    // add to wishlist
+    const addWishlist = (products) => {
+        dispatch(addToWishlist(products))
+        toast.success('Added to wishlist');
+        console.log(products)
     }
 
     useEffect(() => {
         localStorage.setItem('cart', JSON.stringify(cartItems));
     }, [cartItems])
 
-
-
+    useEffect(() => {
+        localStorage.setItem('wishlist', JSON.stringify(wishlistItems));
+    }, [wishlistItems])
 
     return (
         <Layout>
@@ -63,7 +65,7 @@ function ProductInfo() {
                     <div className="lg:w-4/5 mx-auto flex flex-wrap">
                         <img
                             alt="ecommerce"
-                            className="lg:w-1/3 w-full lg:h-auto  object-cover object-center rounded"
+                            className="lg:w-1/3 w-full lg:h-auto object-cover object-center rounded"
                             src={products.imageUrl}
                         />
                         <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
@@ -175,33 +177,33 @@ function ProductInfo() {
                                 {products.description}
                             </p>
 
-                            <div className="flex">
-                                <span className="title-font font-medium text-2xl text-gray-900">
-                                ₹{products.price}
-                                </span>
-                                <button  onClick={()=>addCart(products)} className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                                    Add To Cart
-                                </button>
-                                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                                    <svg
-                                        fill="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        className="w-5 h-5"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </div>}
-                </div>
-            </section>
+<div className="flex">
+<span className="title-font font-medium text-2xl text-gray-900">
+₹{products.price}
+</span>
+<button  onClick={()=>addCart(products)} className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+    Add To Cart
+</button>
+<button onClick={()=>addWishlist(products)} className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+    <svg
+        fill="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        className="w-5 h-5"
+        viewBox="0 0 24 24"
+    >
+        <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
+    </svg>
+</button>
+</div>
+</div>
+</div>}
+</div>
+</section>
 
-        </Layout>
-    )
+</Layout>
+)
 }
 
-export default ProductInfo
+export default ProductInfo;
